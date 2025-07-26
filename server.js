6fb81134c13b2681e9b467acc3b64fa9af7dc083
unknown <hisuyi1987@gmail.com>
2025-07-27 01:42:15 +0800

    修复poloai.top API认证问题

--- server.js
+++ server.js
@@ -141,6 +141,7 @@ async function getNewsFromOpenAI(keywords) {
         max_tokens: 1000,
         temperature: 0.7
       };
+      headers['Authorization'] = `Bearer ${config.openai.apiKey}`;
     } else {
       // 标准OpenAI格式
       requestData = {
